509b84216b4fdc6c74d475ccafbe3c108267e0d8
Raza Agha <Waztaz@gmail.com>
2026-01-30 00:44:34 +0000

    Pulling image: gcr.io/k8s-skaffold/pack Using default tag: latest latest

--- src/app/api/genkit/[...slug]/route.ts
+++ src/app/api/genkit/[...slug]/route.ts
@@ -1,3 +1,4 @@
-import { genkitNextApiHandler } from '@genkit-ai/next';
+import { createNextApiHandler } from '@genkit-ai/next';
+import { ai } from '@/ai/genkit';
 
-export const { GET, POST } = genkitNextApiHandler();
+export const { GET, POST } = createNextApiHandler({ ai });
